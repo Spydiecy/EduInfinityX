45b49e559a1b76bfce6b12c0c0e27b73c6dd13da
Naman Bansal <namanbansal102@gmail.com>
2025-01-05 22:43:58 +0530

    Added Demo Video

--- src/app/demoVideo/page.tsx
+++ src/app/demoVideo/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
          <video width="1000" autoPlay controls>
         <source src="https://firebasestorage.googleapis.com/v0/b/osc-official-b3cab.appspot.com/o/files%2FWhatsApp%20Video%202025-01-05%20at%2010.24.27%20PM.mp4?alt=media&token=c0e973d6-277a-4d62-af03-31d6adaebe2b" type="video/mp4" />
         Your browser does not support the video tag.
+        
       </video>
     </div>
   )
